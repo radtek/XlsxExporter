98f376319bade63b29bca5aac5a23eeddf84c652
iCunda <iCunda@qq.com>
2019-06-05 14:16:31 +0800

    完善界面

--- XlsxExporter/ConfigForm.Designer.cs
+++ XlsxExporter/ConfigForm.Designer.cs
@@ -28,117 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.GroupBox groupBox1;
+            System.Windows.Forms.GroupBox groupBox2;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Panel panel1;
             this._bn_inputBrowse = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this._tb_inputPath = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this._btn_cancel = new System.Windows.Forms.Button();
             this._btn_save = new System.Windows.Forms.Button();
+            this._rb_typeText = new System.Windows.Forms.RadioButton();
+            this._rb_typeBin = new System.Windows.Forms.RadioButton();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            label3 = new System.Windows.Forms.Label();
+            panel1 = new System.Windows.Forms.Panel();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // _bn_inputBrowse
             // 
-            this._bn_inputBrowse.Location = new System.Drawing.Point(307, 27);
+            this._bn_inputBrowse.Location = new System.Drawing.Point(290, 20);
             this._bn_inputBrowse.Name = "_bn_inputBrowse";
             this._bn_inputBrowse.Size = new System.Drawing.Size(57, 21);
-            this._bn_inputBrowse.TabIndex = 7;
+            this._bn_inputBrowse.TabIndex = 2;
             this._bn_inputBrowse.Text = "浏览";
             this._bn_inputBrowse.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "输入路径:";
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(33, 24);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(35, 12);
+            label1.TabIndex = 0;
+            label1.Text = "目录:";
             // 
             // _tb_inputPath
             // 
-            this._tb_inputPath.Location = new System.Drawing.Point(87, 27);
+            this._tb_inputPath.Location = new System.Drawing.Point(74, 20);
             this._tb_inputPath.Name = "_tb_inputPath";
             this._tb_inputPath.Size = new System.Drawing.Size(214, 21);
-            this._tb_inputPath.TabIndex = 8;
+            this._tb_inputPath.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 81);
+            this.textBox1.Location = new System.Drawing.Point(74, 22);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(214, 21);
-            this.textBox1.TabIndex = 8;
+            this.textBox1.TabIndex = 1;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 12);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "输出路径:";
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(33, 26);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(35, 12);
+            label2.TabIndex = 0;
+            label2.Text = "目录:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(307, 81);
+            this.button1.Location = new System.Drawing.Point(290, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(57, 21);
-            this.button1.TabIndex = 7;
+            this.button1.TabIndex = 2;
             this.button1.Text = "浏览";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // _btn_cancel
             // 
-            this._btn_cancel.Location = new System.Drawing.Point(94, 132);
+            this._btn_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._btn_cancel.Location = new System.Drawing.Point(91, 178);
             this._btn_cancel.Name = "_btn_cancel";
             this._btn_cancel.Size = new System.Drawing.Size(75, 23);
-            this._btn_cancel.TabIndex = 10;
+            this._btn_cancel.TabIndex = 3;
             this._btn_cancel.Text = "取消";
             this._btn_cancel.UseVisualStyleBackColor = true;
             // 
             // _btn_save
             // 
-            this._btn_save.Location = new System.Drawing.Point(218, 132);
+            this._btn_save.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this._btn_save.Location = new System.Drawing.Point(244, 178);
             this._btn_save.Name = "_btn_save";
             this._btn_save.Size = new System.Drawing.Size(75, 23);
-            this._btn_save.TabIndex = 11;
+            this._btn_save.TabIndex = 4;
             this._btn_save.Text = "保存";
             this._btn_save.UseVisualStyleBackColor = true;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(this._bn_inputBrowse);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(this._tb_inputPath);
+            groupBox1.Location = new System.Drawing.Point(16, 9);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(381, 55);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "输入";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(panel1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(this.button1);
+            groupBox2.Controls.Add(this.textBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Location = new System.Drawing.Point(16, 77);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(381, 90);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "输出";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(96, 60);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(35, 12);
+            label3.TabIndex = 0;
+            label3.Text = "类型:";
+            // 
+            // _rb_typeText
+            // 
+            this._rb_typeText.AutoSize = true;
+            this._rb_typeText.Checked = true;
+            this._rb_typeText.Location = new System.Drawing.Point(3, 3);
+            this._rb_typeText.Name = "_rb_typeText";
+            this._rb_typeText.Size = new System.Drawing.Size(47, 16);
+            this._rb_typeText.TabIndex = 1;
+            this._rb_typeText.Text = "文本";
+            this._rb_typeText.UseVisualStyleBackColor = true;
+            // 
+            // _rb_typeBin
+            // 
+            this._rb_typeBin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._rb_typeBin.AutoSize = true;
+            this._rb_typeBin.Location = new System.Drawing.Point(76, 3);
+            this._rb_typeBin.Name = "_rb_typeBin";
+            this._rb_typeBin.Size = new System.Drawing.Size(59, 16);
+            this._rb_typeBin.TabIndex = 2;
+            this._rb_typeBin.Text = "二进制";
+            this._rb_typeBin.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(this._rb_typeText);
+            panel1.Controls.Add(this._rb_typeBin);
+            panel1.Location = new System.Drawing.Point(137, 54);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(138, 22);
+            panel1.TabIndex = 3;
+            // 
             // ConfigForm
             // 
+            this.AcceptButton = this._btn_save;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 170);
+            this.CancelButton = this._btn_cancel;
+            this.ClientSize = new System.Drawing.Size(414, 209);
+            this.Controls.Add(groupBox2);
+            this.Controls.Add(groupBox1);
             this.Controls.Add(this._btn_save);
             this.Controls.Add(this._btn_cancel);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this._bn_inputBrowse);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this._tb_inputPath);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ConfigForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "ConfigForm";
+            this.Text = "配置";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button _bn_inputBrowse;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox _tb_inputPath;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button _btn_cancel;
         private System.Windows.Forms.Button _btn_save;
+        private System.Windows.Forms.RadioButton _rb_typeText;
+        private System.Windows.Forms.RadioButton _rb_typeBin;
     }
 }
--- XlsxExporter/MainForm.Designer.cs
+++ XlsxExporter/MainForm.Designer.cs
@@ -28,22 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
             this._bt_editConfig = new System.Windows.Forms.Button();
             this._bt_reloadConfig = new System.Windows.Forms.Button();
             this._bt_export = new System.Windows.Forms.Button();
             this._tb_outPath = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this._bn_outBrowse = new System.Windows.Forms.Button();
             this._lv_taskView = new System.Windows.Forms.ListView();
             this._lb_status = new System.Windows.Forms.Label();
             this._pb_progress = new System.Windows.Forms.ProgressBar();
             this._pb_progressText = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // _bt_editConfig
             // 
             this._bt_editConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this._bt_editConfig.Location = new System.Drawing.Point(414, 8);
+            this._bt_editConfig.Location = new System.Drawing.Point(414, 7);
             this._bt_editConfig.Name = "_bt_editConfig";
             this._bt_editConfig.Size = new System.Drawing.Size(75, 25);
             this._bt_editConfig.TabIndex = 3;
@@ -54,45 +55,45 @@
             // _bt_reloadConfig
             // 
             this._bt_reloadConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this._bt_reloadConfig.Location = new System.Drawing.Point(495, 8);
+            this._bt_reloadConfig.Location = new System.Drawing.Point(495, 7);
             this._bt_reloadConfig.Name = "_bt_reloadConfig";
             this._bt_reloadConfig.Size = new System.Drawing.Size(75, 25);
-            this._bt_reloadConfig.TabIndex = 2;
+            this._bt_reloadConfig.TabIndex = 4;
             this._bt_reloadConfig.Text = "重新载入";
             this._bt_reloadConfig.UseVisualStyleBackColor = true;
             // 
             // _bt_export
             // 
             this._bt_export.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this._bt_export.Location = new System.Drawing.Point(586, 8);
+            this._bt_export.Location = new System.Drawing.Point(586, 7);
             this._bt_export.Name = "_bt_export";
             this._bt_export.Size = new System.Drawing.Size(75, 25);
-            this._bt_export.TabIndex = 1;
+            this._bt_export.TabIndex = 5;
             this._bt_export.Text = "开始输出";
             this._bt_export.UseVisualStyleBackColor = true;
             // 
             // _tb_outPath
             // 
-            this._tb_outPath.Location = new System.Drawing.Point(79, 10);
+            this._tb_outPath.Location = new System.Drawing.Point(79, 9);
             this._tb_outPath.Name = "_tb_outPath";
             this._tb_outPath.Size = new System.Drawing.Size(214, 21);
-            this._tb_outPath.TabIndex = 5;
+            this._tb_outPath.TabIndex = 1;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "输出路径:";
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(14, 13);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(59, 12);
+            label1.TabIndex = 0;
+            label1.Text = "输出目录:";
             // 
             // _bn_outBrowse
             // 
-            this._bn_outBrowse.Location = new System.Drawing.Point(299, 10);
+            this._bn_outBrowse.Location = new System.Drawing.Point(299, 9);
             this._bn_outBrowse.Name = "_bn_outBrowse";
             this._bn_outBrowse.Size = new System.Drawing.Size(57, 21);
-            this._bn_outBrowse.TabIndex = 4;
+            this._bn_outBrowse.TabIndex = 2;
             this._bn_outBrowse.Text = "浏览";
             this._bn_outBrowse.UseVisualStyleBackColor = true;
             // 
@@ -105,7 +106,7 @@
             this._lv_taskView.Location = new System.Drawing.Point(0, 39);
             this._lv_taskView.Name = "_lv_taskView";
             this._lv_taskView.Size = new System.Drawing.Size(684, 377);
-            this._lv_taskView.TabIndex = 9;
+            this._lv_taskView.TabIndex = 6;
             this._lv_taskView.UseCompatibleStateImageBehavior = false;
             // 
             // _lb_status
@@ -116,7 +117,7 @@
             this._lb_status.Location = new System.Drawing.Point(3, 419);
             this._lb_status.Name = "_lb_status";
             this._lb_status.Size = new System.Drawing.Size(29, 12);
-            this._lb_status.TabIndex = 10;
+            this._lb_status.TabIndex = 0;
             this._lb_status.Text = "就绪";
             // 
             // _pb_progress
@@ -125,7 +126,7 @@
             this._pb_progress.Location = new System.Drawing.Point(504, 419);
             this._pb_progress.Name = "_pb_progress";
             this._pb_progress.Size = new System.Drawing.Size(150, 13);
-            this._pb_progress.TabIndex = 11;
+            this._pb_progress.TabIndex = 0;
             // 
             // _pb_progressText
             // 
@@ -134,7 +135,7 @@
             this._pb_progressText.Location = new System.Drawing.Point(655, 419);
             this._pb_progressText.Name = "_pb_progressText";
             this._pb_progressText.Size = new System.Drawing.Size(29, 12);
-            this._pb_progressText.TabIndex = 12;
+            this._pb_progressText.TabIndex = 0;
             this._pb_progressText.Text = "100%";
             // 
             // MainForm
@@ -147,7 +148,7 @@
             this.Controls.Add(this._lb_status);
             this.Controls.Add(this._lv_taskView);
             this.Controls.Add(this._bn_outBrowse);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(label1);
             this.Controls.Add(this._tb_outPath);
             this.Controls.Add(this._bt_export);
             this.Controls.Add(this._bt_reloadConfig);
@@ -165,7 +166,6 @@
         private System.Windows.Forms.Button _bt_reloadConfig;
         private System.Windows.Forms.Button _bt_editConfig;
         private System.Windows.Forms.Button _bn_outBrowse;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox _tb_outPath;
         private System.Windows.Forms.ListView _lv_taskView;
         private System.Windows.Forms.Label _lb_status;
